93b707f88a86a80fe7815d3f3934817ba0b7fb60
huairan-19 <2476437684@qqcom>
2023-10-20 02:11:47 +0800

    init

--- exercises/conversions/using_as.rs
+++ exercises/conversions/using_as.rs
@@ -10,16 +10,23 @@
 // Execute `rustlings hint using_as` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-fn average(values: &[f64]) -> f64 {
+
+fn average(values: &[f64]) -> Option<f64> {
+    if values.is_empty() {
+        return None;
+    }
     let total = values.iter().sum::<f64>();
-    total / values.len()
+    Some(total / (values.len() as f64))
 }
 
 fn main() {
     let values = [3.5, 0.3, 13.0, 11.7];
-    println!("{}", average(&values));
+    if let Some(result) = average(&values) {
+        println!("{}", result);
+    } else {
+        println!("Cannot calculate the average of an empty array.");
+    }
 }
 
 #[cfg(test)]
@@ -28,6 +35,11 @@ mod tests {
 
     #[test]
     fn returns_proper_type_and_value() {
-        assert_eq!(average(&[3.5, 0.3, 13.0, 11.7]), 7.125);
+        assert_eq!(average(&[3.5, 0.3, 13.0, 11.7]).unwrap(), 7.125);
+    }
+
+    #[test]
+    fn returns_none_for_empty_array() {
+        assert!(average(&[]).is_none());
     }
 }
