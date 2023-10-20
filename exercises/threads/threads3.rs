73798a430d9ccfed6174ac04fa272568a0a465f1
huairan-19 <2476437684@qqcom>
2023-10-20 09:03:20 +0800

    init

--- exercises/threads/threads3.rs
+++ exercises/threads/threads3.rs
@@ -1,10 +1,14 @@
+// threads3.rs
+//
+// Execute `rustlings hint threads3` or use the `hint` watch subcommand for a
+// hint.
 use std::sync::mpsc;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
 
 struct Queue {
-    length: usize,
+    length: u32,
     first_half: Vec<u32>,
     second_half: Vec<u32>,
 }
@@ -19,44 +23,43 @@ impl Queue {
     }
 }
 
-fn send_tx(q: Arc<Queue>, tx: Arc<Mutex<mpsc::Sender<u32>>>) {
-    let qc1 = Arc::clone(&q);
-    let qc2 = Arc::clone(&q);
+fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
+    let qc = Arc::new(q);
+    let qc1 = qc.clone();
+    let qc2 = qc.clone();
+    let tx1 = tx.clone();
+    let tx2 = tx.clone();
 
     thread::spawn(move || {
         for val in &qc1.first_half {
             println!("sending {:?}", val);
-            tx.lock().unwrap().send(*val).unwrap();
+            tx1.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
 
-    for val in &qc2.second_half {
-        println!("sending {:?}", val);
-        tx.lock().unwrap().send(*val).unwrap();
-        thread::sleep(Duration::from_secs(1));
-    }
+    thread::spawn(move || {
+        for val in &qc2.second_half {
+            println!("sending {:?}", val);
+            tx2.send(*val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
 }
 
 fn main() {
     let (tx, rx) = mpsc::channel();
-    let queue = Arc::new(Queue::new());
+    let queue = Queue::new();
     let queue_length = queue.length;
 
-    let tx_mutex = Arc::new(Mutex::new(tx));
+    send_tx(queue, tx);
 
-    send_tx(Arc::clone(&queue), Arc::clone(&tx_mutex));
-
-    let mut total_received: usize = 0;
-    for _ in rx {
+    let mut total_received: u32 = 0;
+    for received in rx {
+        println!("Got: {}", received);
         total_received += 1;
-        if total_received == queue_length {
-            break;
-        }
     }
 
     println!("total numbers received: {}", total_received);
-    assert_eq!(total_received, queue_length);
-
-    drop(tx_mutex);
+    assert_eq!(total_received, queue_length)
 }
