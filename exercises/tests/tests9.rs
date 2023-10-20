5130836a539509497f56d94cc6c07298a2b763cb
huairan-19 <2476437684@qqcom>
2023-10-20 09:40:47 +0800

    init

--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -27,7 +27,6 @@
 //
 // You should NOT modify any existing code except for adding two lines of attributes.
 
-
 extern "Rust" {
     #[link_name = "my_demo_function"]
     fn my_demo_function(a: u32) -> u32;
@@ -62,22 +61,3 @@ mod tests {
     }
 }
 
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_success() {
-        // The externally imported functions are UNSAFE by default
-        // because of untrusted source of other languages. You may
-        // wrap them in safe Rust APIs to ease the burden of callers.
-        //
-        // SAFETY: We know those functions are aliases of a safe
-        // Rust function.
-        unsafe {
-            my_demo_function(123);
-            my_demo_function_alias(456);
-        }
-    }
-}
